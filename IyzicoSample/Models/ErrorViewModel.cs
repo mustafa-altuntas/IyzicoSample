4cc61027ffa75cb65d7d87be005806103f758088
altun <altun@PC>
2025-04-03 21:51:47 +0300

    Add project files.

---
+++ IyzicoSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IyzicoSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
